43431afcbc41f60b26d905230a68728b3df0850b
jasmynbarham <70365232+jasmynbarham@users.noreply.github.com>
2020-10-15 13:24:26 -0600

    uploads
    uploads

---
+++ HOMEWORK_9/sketch.js
@@ -0,0 +1,60 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill(15);
+  circle(170,50,110);
+  ellipse(170,70,90,140);
+  fill(90,60,38);
+  circle(170,65,90);
+  line(20,40,200,200);
+  line(300,15,200,160)
+  fill(230)
+  circle(153,60,10);
+  circle(188,60,10);
+  fill(120,89,250);
+  rect(130,110,80,160);
+  fill(120,89,250);
+  rect(108,378,50,10);
+  rect(184,378,50,10);
+  fill(10)
+  ellipse(120,70,10,19);
+  ellipse(220,70,10,19);
+  triangle(170,80,160,80,170,70);
+  ellipse(150,325,30,110);
+  ellipse(190,325,30,110)
+  fill(250)
+  ellipse(170,96,30,10)
+  point(153,60);
+  point(187,60);
+  point(176,85);
+  fill(10);
+  ellipse(170,20,90,20);
+  line(140,52,165,52);
+  line(140,67,162,67);
+  line(140,52,140,67);
+  line(165,52,162,67);
+  //
+  line(175,52,200,52);
+  line(180,67,200,67);
+  line(200,52,200,67);
+  line(175,52,180,67);
+  //
+  line(164,60,178,60);
+  line(140,52,120,52);
+  line(200,52,220,52);
+  ellipse(140,30,10,40);
+  ellipse(152,30,10,30);
+  ellipse(165,30,10,30);
+  ellipse(179,30,10,30);
+  ellipse(194,30,10,30);
+  ellipse(205,30,10,30);
+  textSize(45);
+  text('Jasmyn Barham', 35, 390);
+  textSize(20)
+  text('A Self Portrait', 240,140)
+
+  
+}
